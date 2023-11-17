4d62c9bdb8968bfcb7bca29643f04e728e0836e2
lenairmler <lena.irmler@online.de>
2023-11-17 12:22:56 +0100

    Reset and Remove big File

--- src/app/routes.js
+++ src/app/routes.js
@@ -1,12 +1,14 @@
 import React from "react";
-import { Route, Routes} from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import withRouter from "../hooks/withRouter"
 import { Home } from "../pages/home";
 import { Portfolio } from "../pages/portfolio";
 import { ContactUs } from "../pages/contact";
 import { About } from "../pages/about";
+import Headermain from "../header";
 import { Socialicons } from "../components/socialicons";
 import { CSSTransition, TransitionGroup } from "react-transition-group";
+import { Container, Row, Col } from 'react-bootstrap'
 
 const AnimatedRoutes = withRouter(({ location }) => (
   <TransitionGroup>
@@ -20,11 +22,11 @@ const AnimatedRoutes = withRouter(({ location }) => (
       unmountOnExit
     >
       <Routes location={location}>
-        <Route exact path="/" element={<Home />} />
+        <Route exact path="/" element={<Portfolio />} />
         <Route path="/about" element={<About />} />
         <Route path="/portfolio" element={<Portfolio />} />
         <Route path="/contact" element={<ContactUs />} />
-        <Route path="*" element={<Home />} />
+        <Route path="*" element={<Portfolio />} />
       </Routes>
     </CSSTransition>
   </TransitionGroup>
@@ -33,8 +35,17 @@ const AnimatedRoutes = withRouter(({ location }) => (
 function AppRoutes() {
   return (
     <div className="s_c">
-      <AnimatedRoutes />
-      <Socialicons />
+      <Container>
+        <Row>
+          <Col lg="3" md="3" sm="3">
+            <Headermain />
+            <Socialicons />
+          </Col>
+          <Col>
+            <AnimatedRoutes />
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 }
--- src/pages/portfolio/index.js
+++ src/pages/portfolio/index.js
@@ -1,25 +1,51 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./style.css";
 import { Helmet, HelmetProvider } from "react-helmet-async";
 import { Container, Row, Col } from "react-bootstrap";
 import { dataportfolio, meta } from "../../content_option";
 
 export const Portfolio = () => {
+  // const handleToggle = () => {
+  //   setActive(!isActive);
+  //   document.body.classList.toggle("ovhidden");
+  // };
+
   return (
     <HelmetProvider>
       <Container className="About-header">
-        <Helmet>
+        {/* <Helmet>
           <meta charSet="utf-8" />
-          <title> Portfolio | {meta.title} </title>{" "}
+          <title> Projects | {meta.title} </title>{" "}
           <meta name="description" content={meta.description} />
-        </Helmet>
+        </Helmet> */}
         <Row className="mb-5 mt-3 pt-md-3">
           <Col lg="8">
-            <h1 className="display-4 mb-4"> Portfolio </h1>{" "}
-            <hr className="t_border my-4 ml-0 text-left" />
+            <h1 className="display-4 mmy-4 ml-0b-4"> projects </h1>{" "}
+            <hr className="t_border  text-left" />
           </Col>
         </Row>
-        <div className="mb-5 po_items_ho">
+        <Row>
+          {/* <Col lg="4">
+          <div className="menu__container p-3">
+                <ul className="the_menu">
+                  <li className="menu_item">
+                  <Link  onClick={handleToggle} to="/" className="my-2">Home</Link>
+                  </li>
+                  <li className="menu_item">
+                    <Link  onClick={handleToggle} to="/portfolio" className="my-2"> Portfolio</Link>
+                  </li>
+                  <li className="menu_item">
+                  <Link onClick={handleToggle} to="/about" className="my-2">About</Link>
+                  </li>
+                  <li className="menu_item">
+                  <Link onClick={handleToggle} to="/contact" className="my-2"> Contact</Link>
+                  </li>
+                </ul>
+              </div>
+          </Col> */}
+          <Col lg="8">
+          <div className="mb-5 po_items_ho">
           {dataportfolio.map((data, i) => {
             return (
               <div key={i} className="po_item">
@@ -29,9 +55,11 @@ export const Portfolio = () => {
                   <a href={data.link}>view project</a>
                 </div>
               </div>
-            );
-          })}
-        </div>
+              );
+            })}
+          </div>
+        </Col>
+        </Row>
       </Container>
     </HelmetProvider>
   );
